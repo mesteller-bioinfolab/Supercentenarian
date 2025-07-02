4cad082c6838202a3e142f38246b449b026a8679
MEsteller_BioinfoLab <mcastro@carrerasresearch.org>
2025-07-02 16:07:05 +0200

    Add files via upload

---
+++ GENOMICS_M116_06_make_table_split/command_blood.sh
@@ -0,0 +1,76 @@
+#!/bin/bash
+
+# Main Code --------------------------------------------------------
+
+main() {
+  rm -fR ./real/outputs/blood/*
+ 
+  ANN_KEY=./real/inputs/CSQ_KEY_78
+
+  TISSUE=blood
+
+  # Configuration
+  
+  #module load BCFtools/1.9-foss-2018b  
+  #bcftools query -f '%CHROM\n' ../0-data/${TISSUE}/M116.GATK.snp.vcf.gz  | sort -u > ./real/inputs/chrs.list
+  
+  module load GATK/4.1.6.0-GCCcore-8.3.0-Java-11
+
+  CHR_LIST=../02-vep-annot/real/inputs/chrs.list
+
+  INPUT_DIR=/gpfs42/projects/lab_anavarro/disease_pleiotropies/supercentenarian/Olot/05-make-table/real/outputs
+  OUTPUT_DIR=./real/outputs
+  
+  
+ 
+   IDS=$(ls ${INPUT_DIR}/${TISSUE})
+
+ 
+  # Execution 
+  mkdir ${OUTPUT_DIR}/${TISSUE}
+  #rm -fR ./real/outputs/${TISSUE}
+
+
+  # one job per ID in tissue
+  
+for ID in ${IDS}
+do
+echo $ID
+
+mkdir ${OUTPUT_DIR}/${TISSUE}/${ID}
+
+
+  # COMMAND
+  COMMAND=" \
+    ./real/scripts/ann-split.sh \
+      ${CHR_LIST} \
+      ${INPUT_DIR} \
+      ${OUTPUT_DIR} \
+      ${TISSUE} \
+      ${ID} \
+      ${ANN_KEY}
+  "
+
+  # Cluster array execution
+  JOBS_COUNT=$(cat ${CHR_LIST} | wc -l)
+  eval sbatch --array=1-${JOBS_COUNT} ${COMMAND}
+
+done
+ 
+ exit
+
+
+  # Direct execution
+  #eval bash ${COMMAND}
+  #exit 
+
+  # Cluster execution
+  #eval sbatch ${COMMAND}
+  #exit             
+
+} 
+
+
+# FUNCTIONS ==========================================================
+
+main
